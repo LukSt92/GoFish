39f9fdeda79812ad70915b93a2fbbdab8d721bf7
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-09-10 21:07:54 +0200

    Add: PlayRound method in GameState class

--- GoFish/GameState.cs
+++ GoFish/GameState.cs
@@ -30,9 +30,32 @@ namespace GoFish
             Players = new List<Player>() { HumanPlayer}.Concat(Opponents);
         }
         public Player RandomPlayer(Player currentPlayer) => Players.Where(x => x != currentPlayer).Skip(Player.Random.Next(Players.Count() - 1)).First();
-        public string PlayRound(Player player, Player playerToAsk, Values valueTOAskFor, Deck stock)
+        public string PlayRound(Player player, Player playerToAsk, Values valueToAskFor, Deck stock)
         {
-            throw new NotImplementedException();
+            var valuePlural = (valueToAskFor == Values.Six) ? "Sixes" : $"{valueToAskFor}s";
+            var message = $"{player.Name} asked {playerToAsk.Name}"
+                        + $" for {valuePlural}{Environment.NewLine}";
+            var cards = playerToAsk.DoYouHaveAny(valueToAskFor, stock);
+            if (cards.Count() > 0)
+            {
+                player.AddCardsAndPullOutBooks(cards);
+                message += $"{playerToAsk.Name} has {cards.Count()}"
+                            + $" {valueToAskFor} card{Player.S(cards.Count())}";
+            }
+            else if (stock.Count == 0)
+                message += $"The stock is out of cards";
+            else
+            {
+                player.DrawCard(stock);
+                message += $"{player.Name} drew a card";
+            }
+            if (player.Hand.Count() == 0)
+            {
+                player.GetNextHand(stock);
+                message += $"{Environment.NewLine}{player.Name} ran out of cards,"
+                        + $" drew {player.Hand.Count()} from the stock";
+            }
+            return message;
         }
         public string CheckForWinner()
         {
