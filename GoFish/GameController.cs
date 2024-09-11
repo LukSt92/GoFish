9e1285b736c62e4032fc6e84ac1c06fec8f6f21b
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-09-11 18:30:11 +0200

    Add: GameController class

---
+++ GoFish/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish
+{
+    public class GameController
+    {
+        public static Random Random = new Random();
+
+        private GameState gameState;
+        public bool GameOver { get { return gameState.GameOver; } }
+        public Player HumanPlayer { get { return gameState.HumanPlayer; } }
+        public IEnumerable<Player> Opponents { get { return gameState.Opponents; } }
+        public string Status { get; private set; }
+        public GameController(string humanPlayerName, IEnumerable<string> computerPlayerNames)
+        {
+            throw new NotImplementedException();
+        }
+        public void NextRound(Player playerToAsk, Values valueToAskFor)
+        {
+            throw new NotImplementedException();
+        }
+        private void ComputerPlayersPlayNextRound()
+        {
+            throw new NotImplementedException();
+        }
+        public void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
