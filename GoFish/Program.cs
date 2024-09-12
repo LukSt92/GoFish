bbb5a692572be545597668b37bbf685842ccf3e0
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-09-12 14:55:42 +0200

    Add: Part of main method in Program class which asks Player for settings and after that starts game

--- GoFish/Program.cs
+++ GoFish/Program.cs
@@ -10,6 +10,32 @@ namespace GoFish
     {
         static void Main(string[] args)
         {
+            List<string> namesOfOpponents = new List<string>();
+            bool isSettingsGood = false;
+            Console.Write("Enter your name: ");
+            string humanPlayerName = Console.ReadLine();
+            while (isSettingsGood == false)
+            {
+                Console.Write("Enter the number of computer opponents: ");
+                string numberOfOpponents = Console.ReadLine();
+                if (Int32.TryParse(numberOfOpponents, out int number))
+                {
+                    if (number > 0 && number < 6)
+                    {
+                        for (int i = 0; number > i; i++)
+                        {
+                            namesOfOpponents.Add($"Computer #{i + 1}");
+                            isSettingsGood = true;
+                        }
+                    }
+                    else
+                        Console.WriteLine("The number is wrong, please choose beetwen 1 to 5 opponents.");
+                }
+                else
+                    Console.WriteLine("NUMBERS ONLY!");
+            }
+            GameController gameController = new GameController(humanPlayerName, namesOfOpponents);
+            Console.WriteLine(gameController.Status);
         }
     }
 }
